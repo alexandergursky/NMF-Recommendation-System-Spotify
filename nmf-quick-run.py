1dfd3897e4dde4fd9bb146853661e6ba97f06e70
Alexander Gursky <117600966+alexandergursky@users.noreply.github.com>
2023-03-28 16:48:29 -0400

    Add files via upload

---
+++ nmf-quick-run.py
@@ -0,0 +1,39 @@
+
+import pandas as pd
+
+
+# This cell is for importing the already trained model
+scal_nmf_norm_df = pd.read_csv('sparse5-trained.csv', index_col='Unnamed: 0')
+
+loop_break = str('nmf-quick-run').lower()
+
+print('''
+-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+In order to exit the program, type in: nmf-quick-run
+''')
+
+while True:
+    try:
+        # Get user input and convert to lowercase
+        selecting_artist = input('Enter Artist: ').lower()
+
+        if selecting_artist == loop_break:
+            break
+
+        # Selecting the observation (ignoring case)
+        artist = scal_nmf_norm_df.loc[scal_nmf_norm_df.index.str.lower() == selecting_artist.lower()]
+
+        # Check if artist was found
+        if artist.empty:
+            print('No artist found.')
+            continue
+
+        # Scaler-product the observation to the df to get the similarity scores
+        recommendation = scal_nmf_norm_df.dot(artist.iloc[0])
+
+        # Display the results
+        print(recommendation.nlargest(11))
+
+    except:
+        pass
